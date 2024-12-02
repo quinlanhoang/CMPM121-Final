6e8fd5bd468ce9e2ed8a2e10ccd3a55025b1042c
blujai831 <webmaster@blujai831.dev>
2024-12-01 16:01:51 -0800

    Implement InputManager.

--- src/ui.ts
+++ src/ui.ts
@@ -151,33 +151,188 @@ export async function makeSprite(src: string): Promise<Sprite> {
   return result;
 }
 
+/**
+ * Aggregates key and mouse input events and caches their data
+ * to be checked on-demand. Intended to be attached to Camera.canvas
+ * and flushed once per frame.
+ */
+export interface InputManager {
+  keyHeld(key: string): boolean;
+  keyPressed(key: string): boolean;
+  keyReleased(key: string): boolean;
+  get leftClick(): boolean;
+  get leftClickDrag(): boolean;
+  get rightClick(): boolean;
+  get rightClickDrag(): boolean;
+  get middleClick(): boolean;
+  get middleClickDrag(): boolean;
+  get mouseX(): number;
+  get mouseY(): number;
+  handleEvent(event: Event): void;
+  flush(): void;
+  attach(target?: HTMLElement): void;
+}
+
+/**
+ * Creates an input manager instance. The instance will still need
+ * to be attached to an element after the fact. Whichever element is attached
+ * will handle mouse events, but in order to ensure key events are handled
+ * even for targets that cannot hold focus, `document` will always be chosen
+ * to handle key events.
+ */
+export function makeInputManager(): InputManager {
+  const state = {
+    keysHeldThisFrame: {} as Record<string, boolean>,
+    keysHeldLastFrame: {} as Record<string, boolean>,
+    leftHeld: false,
+    leftClick: false,
+    leftClickDrag: false,
+    rightHeld: false,
+    rightClick: false,
+    rightClickDrag: false,
+    middleHeld: false,
+    middleClick: false,
+    middleClickDrag: false,
+    scrollUp: false,
+    scrollDown: false,
+    mouseX: 0,
+    mouseY: 0
+  };
+  return {
+    keyHeld(key) {
+      return !!state.keysHeldThisFrame[key];
+    },
+    keyPressed(key) {
+      return !state.keysHeldLastFrame[key] &&
+        !!state.keysHeldThisFrame[key];
+    },
+    keyReleased(key) {
+      return !!state.keysHeldLastFrame[key] &&
+        !state.keysHeldThisFrame[key];
+    },
+    get leftClick() {return state.leftClick;},
+    get leftClickDrag() {return state.leftClickDrag;},
+    get rightClick() {return state.rightClick;},
+    get rightClickDrag() {return state.rightClickDrag;},
+    get middleClick() {return state.middleClick;},
+    get middleClickDrag() {return state.middleClickDrag;},
+    get mouseX() {return state.mouseX;},
+    get mouseY() {return state.mouseY;},
+    handleEvent(event) {
+      if (event instanceof KeyboardEvent && event.type == "keydown") {
+        state.keysHeldThisFrame[event.key] = true;
+      } else if (event instanceof KeyboardEvent && event.type == "keyup") {
+        state.keysHeldThisFrame[event.key] = false;
+      } else if (
+        event instanceof MouseEvent &&
+        event.type == "mousedown" &&
+        event.button == 0
+      ) {
+        state.leftHeld = true;
+      } else if (
+        event instanceof MouseEvent &&
+        event.type == "mousedown" &&
+        event.button == 1
+      ) {
+        state.middleHeld = true;
+      } else if (
+        event instanceof MouseEvent &&
+        event.type == "mousedown" &&
+        event.button == 2
+      ) {
+        state.rightHeld = true;
+      } else if (
+        event instanceof MouseEvent &&
+        event.type == "mouseup" &&
+        event.button == 0
+      ) {
+        state.leftHeld = false;
+        state.leftClick = !state.leftClickDrag;
+        state.leftClickDrag = false;
+      } else if (
+        event instanceof MouseEvent &&
+        event.type == "mouseup" &&
+        event.button == 1
+      ) {
+        state.middleHeld = false;
+        state.middleClick = !state.middleClickDrag;
+        state.middleClickDrag = false;
+      } else if (
+        event instanceof MouseEvent &&
+        event.type == "mouseup" &&
+        event.button == 2
+      ) {
+        state.rightHeld = false;
+        state.rightClick = !state.rightClickDrag;
+        state.rightClickDrag = false;
+      } else if (
+        event instanceof MouseEvent &&
+        event.type == "mousemove"
+      ) {
+        state.mouseX = event.offsetX;
+        state.mouseY = event.offsetY;
+        state.leftClickDrag = state.leftHeld;
+        state.rightClickDrag = state.rightHeld;
+        state.middleClickDrag = state.middleHeld;
+      }
+    },
+    flush() {
+      for (const key of Object.keys(state.keysHeldThisFrame)) {
+        state.keysHeldLastFrame[key] = state.keysHeldThisFrame[key];
+      }
+      state.leftClick = false;
+      state.rightClick = false;
+      state.middleClick = false;
+    },
+    attach(target = document.querySelector("canvas")!) {
+      for (const eventType of [
+        "keydown",
+        "keyup"
+      ]) {
+        document.addEventListener(eventType, this.handleEvent.bind(this));
+      }
+      for (const eventType of [
+        "mousedown",
+        "mouseup",
+        "mousemove"
+      ]) {
+        target.addEventListener(eventType, this.handleEvent.bind(this));
+      }
+    }
+  };
+}
+
 export async function testUI(): Promise<void> {
   const camera = makeCamera({
     x: 320, y: 240,
     width: 640, height: 480,
     worldWidth: 640, worldHeight: 480
   });
-  document.body.append(camera.canvas);
-
+  camera.attach();
+  const input = makeInputManager();
+  input.attach(camera.canvas);
   const sprite = await makeSprite("/assets/testbg.png");
   setInterval(() => {
     camera.clear();
     sprite.draw(320, 240, camera);
-  }, 10);
-
-  document.addEventListener("keydown", function (event) {
-    if (event.key == "ArrowDown") {
-      camera.y += 5;
-    } else if (event.key == "ArrowUp") {
-      camera.y -= 5;
-    } else if (event.key == "ArrowLeft") {
-      camera.x -= 5;
-    } else if (event.key == "ArrowRight") {
-      camera.x += 5;
-    } else if (event.key == "]") {
-      camera.zoom *= 1.1;
-    } else if (event.key == "[") {
-      camera.zoom /= 1.1;
+    if (input.keyHeld("ArrowUp")) {
+      camera.y -= 1;
     }
-  });
+    if (input.keyHeld("ArrowDown")) {
+      camera.y += 1;
+    }
+    if (input.keyHeld("ArrowLeft")) {
+      camera.x -= 1;
+    }
+    if (input.keyHeld("ArrowRight")) {
+      camera.x += 1;
+    }
+    if (input.keyHeld("[")) {
+      camera.zoom /= 1.01;
+    }
+    if (input.keyHeld("]")) {
+      camera.zoom *= 1.01;
+    }
+    input.flush();
+  }, 10);
 }
